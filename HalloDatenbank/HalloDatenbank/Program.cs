8602126a258d10569c5871f611414f8559a233a9
André <ruland@outlook.de>
2020-12-07 16:34:19 +0100

    Mit lecker Transactionen

--- HalloDatenbank/HalloDatenbank/Program.cs
+++ HalloDatenbank/HalloDatenbank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace HalloDatenbank
@@ -9,42 +10,82 @@ namespace HalloDatenbank
         {
             Console.WriteLine("Hallo Datenbank");
 
-            var conString = "Server=(localdb)\\MSSQLLOCALDB;Database=Firma;Trusted_Connection=true;";
+            var conString = "Server=(localdb)\\MSSQLLOCALDB;Database=NORTHWND;Trusted_Connection=true;";
 
             try
             {
-                var con = new SqlConnection(conString);
-                con.Open();
-                Console.WriteLine("Datenbankverbindung wurde hergestellt");
-
-                var countCmd = new SqlCommand();
-                countCmd.Connection = con;
-                countCmd.CommandText = "SELECT COUNT(*) FROM Mitarbeiter";
-                object obj = countCmd.ExecuteScalar();
-                if (obj is int c)
-                    Console.WriteLine($"Es wurden {c} Mitarbeiter in der Datenbank gefunden");
-
-
-                var cmd = con.CreateCommand();
-                cmd.CommandText = "SELECT * FROM Mitarbeiter";
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                using (var con = new SqlConnection(conString))
                 {
-                    var nachname = reader.GetString(1);
-                    Console.WriteLine(nachname);
+                    con.Open();
+                    Console.WriteLine("Datenbankverbindung wurde hergestellt");
 
-                    if (reader["Vorname"] != null)
-                        Console.WriteLine($"Vorname: {reader["Vorname"]}");
+                    var countCmd = new SqlCommand();
+                    countCmd.Connection = con;
+                    countCmd.CommandText = "SELECT COUNT(*) FROM Employees";
+                    object obj = countCmd.ExecuteScalar();
+                    if (obj is int c)
+                        Console.WriteLine($"Es wurden {c} Mitarbeiter in der Datenbank gefunden");
 
-                    if (reader["GebDatum"] != null)
+
+                    Console.WriteLine("Suche [Enter für alle]:");
+                    var suche = Console.ReadLine();
+
+                    var cmd = con.CreateCommand();
+                    //BÖSE !!!! cmd.CommandText = "SELECT * FROM Employees WHERE Firstname LIKE '" + suche + "%'";
+                    cmd.CommandText = "SELECT * FROM Employees WHERE Firstname LIKE @search+'%'";
+                    cmd.Parameters.AddWithValue("@search", suche);
+
+
+                    var mitarbeiter = new List<Mitarbeiter>();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader()) //IDisoseable
                     {
-                        var gebDatum = reader.GetDateTime(reader.GetOrdinal("GebDatum"));
-                        Console.WriteLine($"GebDatum: {gebDatum:d}");
-                    }
-                    
-                }
+                        while (reader.Read())
+                        {
+                            var m = new Mitarbeiter()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("EmployeeID")),
+                                Nachname = reader.GetString(1)
+                            };
 
-                con.Close();
+                            if (reader["Firstname"] != null)
+                                m.Vorname = reader["Firstname"].ToString();
+
+                            if (reader["BirthDate"] != null)
+                                m.GebDatum = reader.GetDateTime(reader.GetOrdinal("BirthDate"));
+
+                            m.NewGebDatum = m.GebDatum.AddYears(1);
+
+                            Console.WriteLine($"[{m.Id}] {m.Vorname} {m.Nachname} ({m.GebDatum:d} --> {m.NewGebDatum:d})");
+
+                            mitarbeiter.Add(m);
+
+                        }
+                    }// reader.Dispose(); //--> reader.Close();
+
+                    using (var trans = con.BeginTransaction())
+                    {
+                        //UPDATE command
+                        foreach (var m in mitarbeiter)
+                        {
+                            var updateCmd = con.CreateCommand();
+                            updateCmd.Transaction = trans;
+                            updateCmd.CommandText = "UPDATE Employees SET BirthDate = @newBDate WHERE EmployeeID = @id";
+                            updateCmd.Parameters.AddWithValue("@id", m.Id);
+                            updateCmd.Parameters.AddWithValue("@newBDate", m.NewGebDatum);
+
+                            //if (m.Nachname.StartsWith("B"))
+                            //    throw new System.IO.FileNotFoundException();
+
+                            int affectedRows = updateCmd.ExecuteNonQuery();
+                            if (affectedRows == 1) Console.WriteLine("OK");
+                            else if (affectedRows == 0) Console.WriteLine("Fehler, nichts wurde geändert");
+                            else Console.WriteLine($"PANIK! Es wurden {affectedRows} Rows geändert");
+                        }
+
+                        trans.Commit();
+                    }//default: trans.Rollback();
+                }// con.Close();
 
             }
             catch (SqlException ex)
@@ -56,8 +97,19 @@ namespace HalloDatenbank
                 Console.WriteLine($"Programmfehler: {e.Message}");
             }
 
+
             Console.WriteLine("Ende");
             Console.ReadLine();
         }
     }
+
+    class Mitarbeiter
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime GebDatum { get; set; }
+        public DateTime NewGebDatum { get; set; }
+    }
+
 }
