29a5261c132476ff80395c48331641f102a0c785
André <ruland@outlook.de>
2020-12-08 13:05:07 +0100

    Databinding ist cool

---
+++ HalloBinding/HalloBinding/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace HalloBinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 45);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 45);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(553, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 106);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(468, 35);
+            this.trackBar1.Maximum = 300;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(567, 69);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 50;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 38;
+            this.listBox1.Location = new System.Drawing.Point(12, 329);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 460);
+            this.listBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(288, 329);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(430, 46);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(288, 404);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(975, 407);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(916, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1333, 855);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HalloBinding/HalloBinding/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalloBinding
+{
+    public partial class Form1 : Form
+    {
+        List<Auto> autos = new List<Auto>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox2.DataBindings.Add("Text", textBox1, nameof(TextBox.Text));
+            textBox2.DataBindings.Add(nameof(TextBox.BackColor), textBox1, nameof(TextBox.Text), true);
+
+            label1.DataBindings.Add(nameof(label1.Text), trackBar1, nameof(trackBar1.Value));
+            label1.DataBindings.Add(nameof(label1.Width), trackBar1, nameof(trackBar1.Value), true);
+
+            autos.Add(new Auto() { Id = 1, Hersteller = "Baudi", Modell = "B35", PS = 176, Baujahr = new DateTime(2002, 2, 2) });
+            autos.Add(new Auto() { Id = 2, Hersteller = "Baudi", Modell = "B17", PS = 923, Baujahr = new DateTime(2022, 12, 22) });
+            autos.Add(new Auto() { Id = 3, Hersteller = "Bord", Modell = "G99", PS = 123, Baujahr = new DateTime(2012, 12, 12) });
+            autos.Add(new Auto() { Id = 4, Hersteller = "Boyota", Modell = "T99", PS = 1123, Baujahr = new DateTime(2013, 3, 3) });
+
+            
+            bs.DataSource = autos;
+
+
+            dataGridView1.DataSource = bs;
+            listBox1.DataSource = bs;
+            //listBox1.DisplayMember = "Hersteller";
+            listBox1.Format += ListBox1_Format;
+            comboBox1.DataSource = bs;
+
+            var binding = new Binding(nameof(TextBox.Text), bs,"",true);
+            binding.Format += Binding_Format;
+            label2.DataBindings.Add(binding);
+
+        }
+
+        BindingSource bs = new BindingSource();
+        private void Binding_Format(object sender, ConvertEventArgs e)
+        {
+            if (e.Value is Auto a)
+                e.Value = $"{a.Hersteller} aus dem Jahr {a.Baujahr:d}";
+        }
+
+        private void ListBox1_Format(object sender, ListControlConvertEventArgs e)
+        {
+            if (e.Value is Auto a)
+                e.Value = $"Auto: {a.Hersteller} {a.Modell}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hallo");
+        }
+    }
+
+    class Auto
+    {
+        public int Id { get; set; }
+        public string Hersteller { get; set; }
+        public string Modell { get; set; }
+        public int PS { get; set; }
+        public DateTime Baujahr { get; set; }
+    }
+}
